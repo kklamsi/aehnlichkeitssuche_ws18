975547be17473cb46c3b60fe4d39a1d4f74b829f
kklamsi <kl.klamsi@gmx.at>
2018-11-01 14:27:39 +0100

    test1

--- project_klemensRaoul/src/project_klemensRaoul/AllPairs.java
+++ project_klemensRaoul/src/project_klemensRaoul/AllPairs.java
@@ -1,11 +1,34 @@
 package project_klemensRaoul;
 
 
+//import java.util.ArrayList;
+import java.util.Arrays;
+//import javax.swing.JOptionPane;
+
+
 public class AllPairs {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		int[] r1 = {1,2,4};
+		int[] r2 = {1,3,4,5};
+		int[] r3 = {1,2,4,5,7};
+		int[] r4 = {2,3,5,6,7,9};
+		
+		int[][] data = {{1,2,4}, 
+						{1,3,4,5},
+						{1,2,4,5,7},
+						{2,3,5,6,7,9}
+		};
+
+		/*double threshold = 0;
+		threshold = Double.parseDouble((String)JOptionPane.showInputDialog("Please enter a number between 0 and 1:"));
+		System.out.println(threshold);*/
+		
+		System.out.println(Arrays.toString(data[0]));
+
+		
 		
 	}
+
 }
