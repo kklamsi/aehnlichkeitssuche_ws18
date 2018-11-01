8b903b917235bb356cadae76c164075a937cf857
kklamsi <kl.klamsi@gmx.at>
2018-11-01 15:06:47 +0100

    test3

--- project_klemensRaoul/src/project_klemensRaoul/AllPairs.java
+++ project_klemensRaoul/src/project_klemensRaoul/AllPairs.java
@@ -1,8 +1,7 @@
 package project_klemensRaoul;
 
 
-//import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 //import javax.swing.JOptionPane;
 
 
@@ -10,25 +9,32 @@ public class AllPairs {
 	
 	public static void main(String[] args) {
 		
-		int[] r1 = {1,2,4};
-		int[] r2 = {1,3,4,5};
-		int[] r3 = {1,2,4,5,7};
-		int[] r4 = {2,3,5,6,7,9};
-		
+		// initialize simple dataset:
 		int[][] data = {{1,2,4}, 
 						{1,3,4,5},
 						{1,2,4,5,7},
-						{2,3,5,6,7,9}
-		};
+						{2,3,5,6,7,9}};
 
+
+		// set threshold:
 		/*double threshold = 0;
 		threshold = Double.parseDouble((String)JOptionPane.showInputDialog("Please enter a number between 0 and 1:"));
 		System.out.println(threshold);*/
 		
-		System.out.println(Arrays.toString(data[0]));
-
+		// show data:
+		for(int i = 0; i < data.length; i++) {
+			System.out.println(Arrays.toString(data[i]));
+		}
+		
+		//inverted list:
+		HashMap<Integer, ArrayList<Integer>> invertedList = new HashMap<Integer, ArrayList<Integer>>();
+		
+		for (int i = 0; i < data.length; i++) {           
+	        for(int j = 0 < data[i].length; j++) {
+	        	invertedList.put(i, j);
+	        }
+ 	    }
 		
 		
 	}
-
 }
