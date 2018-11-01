d3173db2a232c24f36d2c2bdcae84420c909dbce
kklamsi <kl.klamsi@gmx.at>
2018-11-01 21:16:26 +0100

    test5

--- project_klemensRaoul/src/project_klemensRaoul/AllPairs.java
+++ project_klemensRaoul/src/project_klemensRaoul/AllPairs.java
@@ -15,21 +15,21 @@ public class AllPairs {
 						{1,2,4,5,7},
 						{2,3,5,6,7,9}};
 		
-		int[] r1 = {1,2,4};
+		/*
+		//int[] r1 = {1,2,4};
 		int[] r2 = {1,3,4,5};
 		int[] r3 = {1,2,4,5,7};
 		int[] r4 = {2,3,5,6,7,9};
 		
-		ArrayList<int[]> data2 = new ArrayList<int[]>();
-		data2.add(r1);
-		data2.add(r2);
-		data2.add(r3);
-		data2.add(r4);
+		ArrayList<Integer> r1 = new ArrayList<Integer>();
+		r1.add(1);
+		//r1.add(2);
+		//r1.add(4);
+		//data2.add(r4);
 		
-		HashMap<String, ArrayList<int[]>> invertedIndex = new HashMap<String, ArrayList<int[]>>();
-		invertedIndex.put("r1", data2.add(r1));
-		
-		System.out.println(Arrays.toString(data2.get(1)));
+		HashMap<String, ArrayList<Integer>> invertedIndex = new HashMap<String, ArrayList<Integer>>();
+		invertedIndex.put("r1", r1);
+		*/
 		
 
 		// set threshold:
@@ -43,17 +43,25 @@ public class AllPairs {
 		}
 		
 		
-		/*inverted list:
-		HashMap<Integer, ArrayList<Integer>> invertedList = new HashMap<Integer, ArrayList<Integer>>();
-		
+		// inverted list:
+		HashMap<Integer, ArrayList<String>> invertedList = new HashMap<Integer, ArrayList<String>>();
+		ArrayList<String> helper = new ArrayList<String>();
+
+		int k = 1;
+
 		for (int i = 0; i < data.length; i++) {
-			ArrayList<Integer> helper = new ArrayList<Integer>();
 			
-	        for (int j = 0; j < data[i].length; j++) {
-	        	invertedList.put(data[i], j);
-	        }
- 	    }*/
+			for (int j = 0; j < data[i].length; j++) {
+				if(data[i][j] == k) {
+					helper.add("r"+(i+1));
+				}
+	        	invertedList.put(data[i][j], helper);       	
+	        }		
+		} 
 		
-		
+		System.out.println("key set: " + invertedList.keySet());
+		System.out.println("rows with " + k + ": " + invertedList.get(1));
+
+	
 	}
 }
