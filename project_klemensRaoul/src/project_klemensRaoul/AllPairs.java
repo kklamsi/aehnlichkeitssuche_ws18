a8c771c9d8d8d87ed3fe3384acdf651ebbd81ea2
kklamsi <kl.klamsi@gmx.at>
2018-11-08 18:52:17 +0100

    cleaned the code

--- project_klemensRaoul/src/project_klemensRaoul/AllPairs.java
+++ project_klemensRaoul/src/project_klemensRaoul/AllPairs.java
@@ -12,90 +12,35 @@ public class AllPairs {
 		
 		long start = System.nanoTime();
 		
-		// initialize simple dataset:
-		int[][] data = {{1,3,5}, 
-						{1,2,3,4},
-						{1,2,4,9,11},
-						{1,3,5,8,9,10,11,12,13,14}};
+		// probably way to complicated...
 		
-		System.out.println(data[0][2]);
-		//ArrayList<Integer>[];
-		
-		int[][] invertedInd = new int[data[data.length-1][data[data.length-1].length-1]][data.length];
-		System.out.println(invertedInd[0][3]);
-		
-		
-		for(int i = 0; i < data.length; i++) {
-			for (int j = 0; j < data[i].length; j++) {
-				invertedInd[data[0][0]][i] = i;
-			}
+		List<List<Integer>> dataA = new ArrayList<List<Integer>>();
+		dataA.add(Arrays.asList(1,3,5));
+		dataA.add(Arrays.asList(1,2,3,4));
+		dataA.add(Arrays.asList(1,2,4,9,11));
+		dataA.add(Arrays.asList(1,3,5,8,9,10,11,12,13,14));
+		for (List<Integer> liste : dataA) {
+		    for (Integer i : liste) {
+		        System.out.print(i+" ");
+		    }
+		    System.out.println();
 		}
 		
-		System.out.println(invertedInd[1][0]);
+		System.out.println(dataA.get(3).size());
+		System.out.println(dataA.get(3).get(9));
+		
+		List<SortedSet<Integer>> index = new ArrayList<SortedSet<Integer>>(); 
 		
 		/*
-		//int[] r1 = {1,3,5};
-		int[] r2 = {1,2,3,4};
-		int[] r3 = {1,2,4,9,11};
-		int[] r4 = {1,3,5,8,9,10,11,12,13,14};
-		
-		ArrayList<Integer> r1 = new ArrayList<Integer>();
-		r1.add(1);
-		//r1.add(2);
-		//r1.add(4);
-		//data2.add(r4);
-		
-		HashMap<String, ArrayList<Integer>> invertedIndex = new HashMap<String, ArrayList<Integer>>();
-		invertedIndex.put("r1", r1);
+		for(int i = 0; i < dataA.size(); i++) {
+			for (int j = 0; j < dataA.get(i).size(); j++) {
+				index.get(j).add(SortedSet.add(i));
+			}
+		}
 		*/
 		
-
-		// set threshold:
-		/*double threshold = 0;
-		threshold = Double.parseDouble((String)JOptionPane.showInputDialog("Please enter a number between 0 and 1:"));
-		System.out.println(threshold);*/
-		
-		// show data:
-		for(int i = 0; i < data.length; i++) {
-			System.out.println(Arrays.toString(data[i]));
-		}
-		
-		
-		// inverted list:
-		
-			/*
-		  	Map<String, Character> myNewHashMap = new HashMap<>();
-			for(Map.Entry<Character, String> entry : myHashMap.entrySet()){
-    		myNewHashMap.put(entry.getValue(), entry.getKey());
-			}
-		 	https://stackoverflow.com/questions/20412354/reverse-hashmap-keys-and-values-in-java
-		 	*/
-		
-
-
-		
-		HashMap<Integer, SortedSet<String>> invertedList = new HashMap<Integer, SortedSet<String>>();
-		SortedSet<String> helper = new TreeSet<>();
-
-		//int k = 1;
-		for (int k = 1; k < 15; k++) {
-		for (int i = 0; i < data.length; i++) {
 			
-			for (int j = 0; j < data[i].length; j++) {
-				if(data[i][j] == 1) {
-					helper.add("r"+(i+1));
-				}
-	        	invertedList.put(data[i][j], helper);       	
-	        }	
-		} 
-		}
-		
 		long end = System.nanoTime();
-		
-		int k = 1;
-		
-		System.out.println("key set: " + invertedList.keySet());
-		System.out.println("rows with " + k + ": " + invertedList.get(1));
 		System.out.println((end - start)*0.000000001 + " Sekunden");
 
 	
